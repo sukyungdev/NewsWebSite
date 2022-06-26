8cdb425958384b5a977cc8b403b70487b8c12542
sukyungdev <sukyung.developer@gmail.com>
2022-06-26 23:57:28 +0900

    Code refactoring

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let news = [];
+let url = null;
 const menuTopic = document.querySelectorAll(".menubar a");
 const inputArea = document.querySelector("#input-form input");
 const inputBtn = document.querySelector("#input-form button");
@@ -7,18 +8,39 @@ menuTopic.forEach((item) => {
   item.addEventListener("click", (event)=>{findNewsByMenuTopic(event)});
 });
 inputBtn.addEventListener("click", (event)=>{searchByUserInput(event)});
-// function creation
-// receive news
-async function receiveLatestHeadLineNews(){
-  let header = new Headers({'x-api-key' : 'Cjzdcrz9zloEr6VYyhq7JYFd0eBePABU2irDG-3MN0g'});
-  let url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=12');
 
+// function creation
+
+//request news data
+async function requestNewsData() {
+  let header = new Headers({'x-api-key' : 'Cjzdcrz9zloEr6VYyhq7JYFd0eBePABU2irDG-3MN0g'});
   let requestNews = await fetch(url, {headers : header});
   let receiveNews = await requestNews.json();
   console.log(receiveNews);
   news = receiveNews.articles;
   renderNews();
+}
+// receive news
+const latestHeadLineNews = () => {
+  url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=12');
+  requestNewsData();
 };
+
+
+// search by menu topic
+const findNewsByMenuTopic = async(event) => {
+  let topic = event.target.textContent;
+  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=12`);
+  requestNewsData();
+};
+
+// search by user input
+const searchByUserInput = async() => {
+  let userInput = inputArea.value.toLowerCase();
+  url = new URL(`https://api.newscatcherapi.com/v2/search?q=${userInput}&countries=KR&page_size=12`);
+  requestNewsData();
+};
+
 
 // render news HTML
 const renderNews = () => {
@@ -34,7 +56,7 @@ const renderNews = () => {
                 src="${news[i].media}"
                 alt="news-img"
             />
-            <h2 class="news-title">${news[i].title}</h2>
+            <h2 class="news-title"><a href="${news[i].link}" target="_blank">${news[i].title}</a></h2>
             <p>
             ${news[i].summary == null || news[i].summary == "" ? "내용없음" 
             : news[i].summary.length > 150 ? news[i].summary.substring(0, 150) + "..." 
@@ -50,35 +72,5 @@ const renderNews = () => {
   document.querySelector(".news .row").innerHTML = renderNewsHTML;
 };
 
-// search by menu topic
-const findNewsByMenuTopic = async(event) => {
-  let topic = event.target.textContent;
-  let header = new Headers({'x-api-key' : 'Cjzdcrz9zloEr6VYyhq7JYFd0eBePABU2irDG-3MN0g'});
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=12`);
-
-  let requestNews = await fetch(url, {headers : header});
-  let receiveNews = await requestNews.json();
-
-  news = receiveNews.articles;
-  console.log(news);
-
-  renderNews();
-};
-
-// search by user input
-const searchByUserInput = async() => {
-  let userInput = inputArea.value.toLowerCase();
-  let header = new Headers({'x-api-key' : 'Cjzdcrz9zloEr6VYyhq7JYFd0eBePABU2irDG-3MN0g'});
-  let url = new URL(`https://api.newscatcherapi.com/v2/search?q=${userInput}&countries=KR&page_size=12`);
-
-  let requestNews = await fetch(url, {headers : header});
-  let receiveNews = await requestNews.json();
-  console.log(receiveNews);
-  news = receiveNews.articles;
-  renderNews();
-}
-
-
-
 // function call
-receiveLatestHeadLineNews();
+latestHeadLineNews();
